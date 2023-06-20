76676f403c921450b1a3f72fa4aaec1c81a4ad74
Duc Pham <>
2023-06-20 20:53:58 +0700

    homework

--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(isValid("()"))     // true
-	fmt.Println(isValid("()[]{}")) // true
-	fmt.Println(isValid("(]"))     // false
-	fmt.Println(isValid("([)]"))   // false
-	fmt.Println(isValid(")"))      // false
-	fmt.Println(isValid("([)"))
+	fmt.Println(isValidParentheses("()"))       // true
+	fmt.Println(isValidParentheses("()[]{}"))   // true
+	fmt.Println(isValidParentheses("(]"))       // false
+	fmt.Println(isValidParentheses("([)]"))     // false
+	fmt.Println(isValidParentheses("{[]}"))     // true
+	fmt.Println(isValidParentheses("(([])[])")) // true
+
+	fmt.Println(containsDuplicate([]int{1, 2, 3}))          // false
+	fmt.Println(containsDuplicate([]int{4, 5, 6}))          // false
+	fmt.Println(containsDuplicate([]int{1, 1, 3, 4, 5, 4})) // true
+	fmt.Println(containsDuplicate([]int{2, 1, 3, 4, 5}))    // false
+	fmt.Println(containsDuplicate([]int{9, 8, 6, 9}))       // true
 }
-func isValid(s string) bool {
+
+func containsDuplicate(nums []int) bool {
+	storage := map[int]bool{}
+	for _, num := range nums {
+		if storage[num] {
+			return true
+		}
+		storage[num] = true
+	}
+	return false
+}
+func isValidParentheses(s string) bool {
 	stack := make([]rune, 0)
 	for _, char := range s {
 		switch char {
@@ -19,25 +36,25 @@ func isValid(s string) bool {
 		case ')':
 			previous := stack[len(stack)-1]
 			if previous == '(' {
-				stack = append(stack, char)
+				stack = stack[:len(stack)-1]
 			} else {
 				return false
 			}
 		case '}':
 			previous := stack[len(stack)-1]
 			if previous == '{' {
-				stack = append(stack, char)
+				stack = stack[:len(stack)-1]
 			} else {
 				return false
 			}
 		case ']':
 			previous := stack[len(stack)-1]
 			if previous == '[' {
-				stack = append(stack, char)
+				stack = stack[:len(stack)-1]
 			} else {
 				return false
 			}
 		}
 	}
-	return len(stack)%2 == 0
+	return len(stack) == 0
 }
